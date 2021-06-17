e61a832d6c221c8e427c5bbaf8d11930f78dba81
Brian <Briann4523@gmail.com>
2021-06-17 13:11:43 -0400

    Just a lot of testing to get used to the API.

---
+++ Bot.lua
@@ -0,0 +1,31 @@
+local discordia = require('Discordia')
+local json = require('json')
+local client = discordia.Client()
+local appealweb = "794419007633096725"
+local appealtable = {}
+
+-- any method that takes discord ids are strings
+
+function timer(message)
+end
+
+function wait(a) 
+    local sec = tonumber(os.clock() + a); 
+    while (os.clock() < sec) do 
+    end 
+end
+
+client:on("messageCreate", function(message)
+    if message.webhookId == appealweb then
+        message:addReaction("👍")
+        message:addReaction("👎")
+        register()
+    end
+end)
+
+client:on("ready", function()
+    wait(2)
+    print(client:getChannel("759254539139285063"))
+end)
+
+client:run("Bot ODU1MDg0NTY4NDA3MDQ4MTky.YMtVjw.FZ4ipkOsMkAq2Z24iAcXC-68rCY")
